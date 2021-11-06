54e6f5d4e78aa1637db2df2b1e5c5fa00f1429da
michellecaner <michellecaner@gmail.com>
2021-11-06 12:34:15 -0500

    code redux

--- flowers.js
+++ flowers.js
@@ -14,6 +14,16 @@ const flowers = [
 ]
 
 const addFlower = (flowerObject) => {
+    // Find what the last or max ID is
+    const indexNum = flowers.length - 1
+    let maxID = flowers[indexNum].id
+    // Add 1 to max ID
+    const newID = maxID + 1
+    // Make the new ID
+    // Add ID property to object
+    flowerObject.id = newID
+    // Add flower object to Flowers array
+    flowers.push(flowerObject)
     /*
         This function should add an `id` property
         to the object that was sent to the function.
@@ -25,16 +35,15 @@ const addFlower = (flowerObject) => {
         object, the object should then be put into the
         `flowers` array.
     */
-        const lastIndex = flowers.length -1
-        const currentLastFlower = flowers[lastIndex]
-        const maxID = currentLastFlower.id
-        const idForNewFlower = maxID + 1
-          
-        flowerObject.id = idForNewFlower
-        flowers.push(flowerObject)
-
+    
 }
+addFlower({
+    color: "Orange",
+    species: "Tulip",
+    price: 0.95
+})
 
+console.log(flowers)
 
 const findExpensiveFlowers = () => {
     const expensiveFlowers = []  // Do not change this code
@@ -46,14 +55,11 @@ const findExpensiveFlowers = () => {
         greater than or equal to 1.00, it should be
         added to the `expensiveFlowers` array.
     */
-for (let i=0; i < flowers.length; i++) {
-    if (flowers[i].price >= 1.00)
-    expensiveFlowers.push(flowers)
-
-}
-
-
-
+        for (let i=0; i < flowers.length; i++) {
+            if (flowers[i].price >= 1.00)
+            expensiveFlowers.push(flowers)
+        
+        }
     return expensiveFlowers  // Do not change this code
 }
 
